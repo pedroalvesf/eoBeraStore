a6aa84371352a14547ab6e7d91189eb6fa992b46
PedroAlvesf <pedro.alves.hf@gmail.com>
2022-11-14 17:42:49 -0300

    starting table

---
+++ script.js
@@ -0,0 +1,16 @@
+
+/* <li class="list-type">
+<img class="pri" src="https://i.imgur.com/eDnel23.png">
+</li>
+<li class="list-type">
+   <img class="pri" src="https://i.imgur.com/xBrqtZx.png">
+</li>
+<li class="list-type">
+   <img class="pri" src="https://i.imgur.com/o9Sub15.png">
+</li>
+<li class="list-type">
+   <img class="pri" src="https://i.imgur.com/Z67nTWp.png">
+</li>
+<li class="list-type">
+   <img class="pri" src="https://i.imgur.com/qzyRsro.png">
+</li> */
