f384b6e5cc49474724defd5baa0f3c0e5b68efec
PedroAlvesf <pedro.alves.hf@gmail.com>
2022-11-14 19:40:08 -0300

    avancando na store

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 
 const containerItens = document.querySelector(".container-first");
+const btnNextSkinGoal = document.querySelector(".new-skin-button")
+const weaponsListTest = [{ 
+    weaponLink: "src(awp-g.png)",
+    weaponName: "AWP",
+    skinLink: "src(awp-g.png)",
+    skinName: "Gungnir",
+    weaponImg: "src(awp-g.png)",
+    weaponPrice: "1000",
+    weaponPriceStattrack: "5000",
+    weaponCollection: "The Norse Collection",
+    inspectInGameLink: "#"
+}
+]
 
 function createCard(weaponLink, weaponName, skinLink, skinName, weaponImg, weaponPrice, weaponPriceStattrack, weaponCollection, inspectInGameLink){
- `   <div class="card-item">
+ const html = `<div class="card-item">
     <h3>
         <a href="${weaponLink}">${weaponName} | </a>
         <a href="${skinLink}">${skinName}</a>
@@ -21,10 +34,31 @@ function createCard(weaponLink, weaponName, skinLink, skinName, weaponImg, weapo
         <p>${weaponCollection}</p>
     </div>
     <div class="btn-inspect">
-    <p><a href="${inspectInGameLink}">Inspect in game</a></p>
-    <p><a href="#">ISteam linking</a></p>
+    <p><a  class="text-btn-inspect" href="${inspectInGameLink}">Inspect in game</a></p>
+    <p><a  class="text-btn-inspect" href="#">ISteam linking</a></p>
     </div>
 </div>`
+return html;
 }
 
+console.log(btnNextSkinGoal)
+btnNextSkinGoal.addEventListener("click", () =>{
+    console.log(weaponsListTest)
+    const insertItem = 
+        createCard(
+        weaponsListTest[0].weaponLink, 
+        weaponsListTest[0].weaponName,
+        weaponsListTest[0].skinLink, 
+        weaponsListTest[0].skinName,
+        weaponsListTest[0].weaponImg, 
+        weaponsListTest[0].weaponPrice, 
+        weaponsListTest[0].weaponPriceStattrack, 
+        weaponsListTest[0].weaponCollection, 
+        weaponsListTest[0].inspectInGameLink);
+        containerItens.insertAdjacentHTML('afterBegin', insertItem);
+})
+
+
 //237|GQbbdWPZmMmjZiUUT7oFIkfYsTJ4SHXAte5Eny8g
+
+// https://steamcommunity.com/market/priceoverview/?country=BR&currency=7&appid=730&market_hash_name=P250%20%7C%20Asiimov%20(Minimal%20Wear)
